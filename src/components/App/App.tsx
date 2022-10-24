494d9ab6134ba2797968b83bccc2f2016d094dc1
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-10-24 23:13:37 +0200

    added fonts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import AddElementPanel from '../AddElementPanel/AddElementPanel';
 import Card from '../card/Card';
 import CustomizationPanel from '../CustomizationPanel/CustomizationPanel';
-
+import './App.scss'
 
 const App = () => {
   return (
     <div className="app">
+      <h1>Example text</h1>
       <CustomizationPanel />
       <Card />
       <AddElementPanel />
